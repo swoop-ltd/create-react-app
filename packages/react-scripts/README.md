a5d8fb06ba2ba7fc7d9fff8507697bc499c111f6
karim-sheikh <60742748+karim-sheikh@users.noreply.github.com>
2020-03-31 18:03:49 +0100

    Init

--- packages/react-scripts/README.md
+++ packages/react-scripts/README.md
@@ -5,3 +5,26 @@ Please refer to its documentation:
 
 - [Getting Started](https://facebook.github.io/create-react-app/docs/getting-started) – How to create a new app.
 - [User Guide](https://facebook.github.io/create-react-app/) – How to develop apps bootstrapped with Create React App.
+
+# @swoop-ltd/react-scripts
+
+The following customisations are added as a part of swoop.
+
+## CspHtmlPlugin
+
+- To whitelist the content sources, a set of content security configs are added to the HTML meta tag. The configs can be found in
+
+  `react-scripts/config/cspConfig.js`
+
+  In `react-scripts/config/webpack.config.js`, add the below to the plugin section
+
+  ```
+  new CspHtmlWebpackPlugin(cspConfig, {
+        enabled: true,
+        hashingMethod: 'sha256',
+        nonceEnabled: {
+          'script-src': false,
+          'style-src': false,
+        },
+      }),
+  ```
---
+++ packages/react-scripts/config/cspConfig.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const cspConfigProd = {
+  'default-src': "'self'",
+  'base-uri': "'self'",
+  'object-src': "'none'",
+  'script-src': [
+    "'self'",
+    "'unsafe-eval'",
+    'https://www.google-analytics.com/analytics.js',
+    'https://www.google-analytics.com/analytics_debug.js',
+    'https://widget.intercom.io',
+    'https://js.intercomcdn.com',
+    'https://prod.webpu.sh',
+  ],
+  'style-src': [
+    "'self'",
+    "'unsafe-inline'",
+    'https://fonts.googleapis.com/css',
+  ],
+  'img-src': [
+    "'self'",
+    'data:',
+    'https://*.blob.core.windows.net',
+    'https://static.intercomassets.com',
+    'https://js.intercomcdn.com',
+    'https://downloads.intercomcdn.com',
+    'https://gifs.intercomcdn.com',
+  ],
+  'font-src': [
+    "'self'",
+    'data:',
+    'https://fonts.gstatic.com',
+    'https://js.intercomcdn.com/fonts/',
+    'https://use.typekit.net',
+  ],
+  'connect-src': [
+    "'self'",
+    'https://*.swoopfunding.com/hawk/api/',
+    'https://*.google-analytics.com/',
+    'https://*.intercom.io',
+    'https://uploads.intercomcdn.com',
+    'wss://*.intercom.io',
+    'https://sentry.io',
+    'https://api.xtremepush.com',
+  ],
+  'media-src': ['https://js.intercomcdn.com'],
+  'frame-src': ['https://www.google.com'],
+};
+
+let cspConfig = cspConfigProd;
+
+if (process.env.NODE_ENV !== 'production') {
+  cspConfig = {
+    ...cspConfigProd,
+    'script-src': [
+      ...cspConfigProd['script-src'],
+      "'unsafe-eval'",
+      'http://localhost:5000',
+    ],
+    'style-src': [...cspConfigProd['style-src'], 'http://localhost:5000'],
+    'img-src': [...cspConfigProd['img-src'], 'http://localhost:5000'],
+    'font-src': [...cspConfigProd['font-src'], 'http://localhost:5000'],
+    'connect-src': [
+      ...cspConfigProd['connect-src'],
+      'http://*:*/sockjs-node/',
+      'ws://*:*/sockjs-node/',
+      'localhost:*',
+    ],
+  };
+}
+
+module.exports = cspConfig;
